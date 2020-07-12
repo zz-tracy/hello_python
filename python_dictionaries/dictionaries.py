7c5b4061255837078a95e03e8e3af74cd3e964e0
zz-tracy <1071793651@qq.com>
2020-07-12 22:34:22 +0800

    operation_dictionar

--- python_dictionaries/dictionaries.py
+++ python_dictionaries/dictionaries.py
@@ -20,3 +20,80 @@ print('The alien is ' + alien_0['color'] + '.')
 
 alien_0['color'] = 'yellow'
 print('The alien is now ' + alien_0['color'] + '.')
+
+# 删除键值-对
+
+alien_0 = {'color': 'green', 'opions': 5}
+print(alien_0)
+
+del alien_0['opions']
+print(alien_0)
+
+
+# 遍历字典
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for key, value in user_0.items():
+    print('\nkey: ' + key)
+    print('value: ' + value)
+
+
+# 遍历字典中的所有键
+
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name in favorite_language.keys():
+    print(name.title())
+
+for name in favorite_language:  # 遍历字典时,会默认遍历所有的键,所以不需要加keys()输出效果一样
+    print(name.title())
+
+
+# 使用sorted()方法按顺序遍历字典中所有的键
+
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name in sorted(favorite_language.keys()):
+    print(name.title() + ', thank you for taking the poll')
+
+
+# 遍历字典中所有的值:
+
+
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for language in favorite_language.values():
+    print(language.title())
+
+
+# 使用集合set()方法找出列表中独一无二的元素
+
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for language in set(favorite_language.values()):
+    print(language.title())
