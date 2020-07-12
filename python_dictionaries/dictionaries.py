482bec39b6ac7c8d3fc4e09b9884ce875e57c11b
zz-tracy <1071793651@qq.com>
2020-07-12 23:00:42 +0800

    opration_dictionary

--- python_dictionaries/dictionaries.py
+++ python_dictionaries/dictionaries.py
@@ -97,3 +97,70 @@ favorite_language = {
 
 for language in set(favorite_language.values()):
     print(language.title())
+
+
+# 嵌套:将一系列字典存储在列表中, 或将列表作为值存储在字典中
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = ['alien_0', 'alien_1', 'alien_2']
+
+for alien in aliens:
+    print(alien)
+
+# 使用range()方法自动生成一些列数字
+# 创建一个空列表用来存储生成的数字
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in alien[:5]:    # 显示前五个生成的信息
+    print(alien)
+print('...')
+
+# 显示创建了多少个数字
+print('Toal number of aliens: ' + str(len(aliens)))
+
+
+# 字典中存储列表
+
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+}
+
+for name, languages in favorite_languages.items():
+    print('\n' + name.title() + '\'s favorite languages are: ')
+    for language in languages:
+        print('\t' + language.title())
+
+
+# 在字典中存储字典
+
+users = {
+    'asinstein' :  {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    }
+}
+
+
+for username, user_info in users.items():
+    print('\nUsername: ' + username)
+    full_name = user_info['first'] + ' ' + user_info['last']
+    location = user_info['location']
+
+    print('\tFull name: ' + full_name.title())
+    print('\tLocation: ' + location.title())
