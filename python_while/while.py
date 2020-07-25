d7be40c05137f4cf3fcb24d289c355617116f440
zz-tracy <1071793651@qq.com>
2020-07-26 01:03:31 +0800

    使用while循环处理列表和字典

--- python_while/while.py
+++ python_while/while.py
@@ -1,48 +1,48 @@
 # 使用While循环
 
-# current_number = 1
-# while current_number <= 5:
-#     print(current_number)
-#     current_number += 1
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
 
 
 # 设定一个退出值,当用户输入该值时退出程序
 
-# prompt = "\nTell me something, and I will repeat it back to you:"
-# prompt += "\nEnter 'quit' to end the program. "
-# message = ''
-# while message != 'quit':
-#     message = input(prompt)
-#     print(message)
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ''
+while message != 'quit':
+    message = input(prompt)
+    print(message)
 
 
 # 使用标志
 
-# prompt = "\nTell me something, and I will repeat it back to you:"
-# prompt += "\nEnter 'quit' to end the program. "
-#
-# active = True   # 标志
-# while active:
-#     message = input(prompt)
-#
-#     if message == 'quit':
-#         active = False
-#     else:
-#         print(message)
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True   # 标志
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
 
 
 # 使用break退出循环
 
-# prompt = "\nTell me something, and I will repeat it back to you:"
-# prompt += "\nEnter 'quit' to end the program. "
-#
-# while True:
-#     city = input(prompt)
-#
-#     if city == 'quit':
-#         break
-#     else:
-#         print('I\'d love to go to ' + city.title() + '!')
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print('I\'d love to go to ' + city.title() + '!')
 
 
 # 在循环中使用continue
