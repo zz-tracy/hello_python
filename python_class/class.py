3347cb83c8f05901eeacbe6b16548555866ae70b
zz-tracy <1071793651@qq.com>
2020-08-18 01:11:19 +0800

    使用类和实例

--- python_class/class.py
+++ python_class/class.py
@@ -38,3 +38,75 @@ my_dog.sit()
 print('\nYour dog\'s name is ' + your_dog.name.title() + '.')
 print('Your dog is ' + str(your_dog.age) + 'years old.')
 your_dog.sit()
+
+
+# 给属性指定默认值
+class Car():
+
+    def __init__(self, make, model, year):
+        """初始化描述汽车的属性"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""
+        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+print(my_new_car.read_odometer())
+
+
+# 修改属性的值
+my_new_car.odometer_reading = 23
+print(my_new_car.read_odometer())
+
+# 通过方法修改属性的值
+class Car():
+    def __init__(self, make, model, year):
+        """初始化描述汽车的属性"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """打印一条指出汽车里程的消息"""
+        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+
+    def update_odometer(self, mileage):
+        """将里程表读数设置为指定的值"""
+        self.odometer_reading = mileage
+
+    # 通过方法对属性的值进行递增
+    def increment_odometer(self, miles):
+        """将里程表读数增加指定的量"""
+        self.odometer_reading += miles
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
