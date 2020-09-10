5c9235404b5d112b4cd14ba676cbff18a3385504
zz-tracy <1071793651@qq.com>
2020-09-10 23:23:18 +0800

    存储数据和读取数据

--- python_file/file.py
+++ python_file/file.py
@@ -239,3 +239,20 @@ with open(filename) as f_obj:
     username = json.load(f_obj)
     print('Welcome back, ' + username + '!')
 
+
+# 使用try--except--else保存和读取用户生成的数据
+filename = r'C:\Users\hp\Desktop\demo\username.json'
+try:
+    with open(filename) as f_obj:
+        username = json.load(f_obj)
+except FileNotFoundError:
+    username = input('What is your name? ')
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+        print('We\'ll remember you when you come back, ' + username + '!')
+else:
+    print('Welcome back, ' + username + '!')
+
+
+
+
