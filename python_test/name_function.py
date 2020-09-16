a62e47d60577be0386be388c986584eccf28a9a2
zz-tracy <1071793651@qq.com>
2020-09-17 01:36:09 +0800

    测试类

--- python_test/name_function.py
+++ python_test/name_function.py
@@ -1,7 +1,10 @@
 """测试代码"""
 
 
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle=''):
     """生成整洁的姓名"""
-    full_name = first + ' ' + middle + ' ' + last
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
     return full_name.title()
--- python_test/test_name_function.py
+++ python_test/test_name_function.py
@@ -14,5 +14,11 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
 
+    def test_first_last_middle_name(self):
+        """能够正确地处理像Wolfgang Amadeus Mozart这样的姓名吗?"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 
 unittest.main()
+
